857115e559f21ea49f8fafa5d899a19e4fd0e6c9
Lenine Pestana <leninepestana@gmail.com>
2019-09-01 19:23:26 +0100

    Final project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insert a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent " + path.getParent());
+		System.out.println("getPath " + path.getPath());
+		
+		sc.close();
 	}
 
 }
